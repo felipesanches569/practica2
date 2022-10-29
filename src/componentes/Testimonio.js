a9d9e4bf7abb9613a6384be9f4c98b3b3c051d29
felipesanches569 <felipesanches_@hotmail.com>
2022-10-28 22:53:47 -0500

    ssQS

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 function Testimonio() {
   return (
     <div className='contenedor-testimonio'>
@@ -6,6 +7,12 @@ function Testimonio() {
         className='imagen-testimonio'
         src={require('../imagenes/1.jpg')}
         alt='foto de junior'/>
+        <div className='contenedor-texto-testimonio'>
+          <p className='nombre-testimonio'>JUAN ROLDAN en bogota</p>
+          <p className='cargo-testimonio'>Ingenieria de software en WINDOWS</p>
+          <p className='texto-testimonio'>¡Hola a todos! Esperamos que les guste el nuevo curso. En la descripción pueden encontrar un enlace al código de los proyectos en GitHub y a freeCodeCamp en Español. Los invitamos a compartir sus comentarios. 👍 ¿Listos para aprender React?</p>
+        </div>
     </div>
   );
 }
+export default Testimonio;
